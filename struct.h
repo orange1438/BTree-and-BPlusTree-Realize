895dc33139114a448cb659189ca9e9bfc7da1115
orange1438 <karl.zhao@qq.com>
2015-09-07 23:41:06 +0800

    Add Storage Struct

--- struct.h
+++ struct.h
@@ -1,11 +1,24 @@
 #pragma once
 // the degree of btree
-#define M 2   
-typedef struct btree_node {
+#define M 2 
+
+typedef struct btree_nodes {
 	int k[2*M-1];
-	struct btree_node *p[2*M];
+	struct btree_nodes *p[2*M];
 	int num;
 	bool is_leaf;
-	struct btree_node *prev;  // use one struct just for simple,这2个，供B+Tree used
-	struct btree_node *next;  // 供B+Tree used
+	struct btree_nodes *prev;  // use one struct just for simple,这2个，供B+Tree used
+	struct btree_nodes *next;  // 供B+Tree used
+	
 } btree_node;
+
+
+typedef struct StorageNode{
+	btree_node bnode;
+	int index[M];   // 索引集合:index_set 
+}storage_node;
+
+typedef struct StorageStruct{
+	storage_node *snode;
+	int len;
+}storage_struct;
